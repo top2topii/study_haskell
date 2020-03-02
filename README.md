b099bd2a2c622d6b19d6499a2388409e49288aab
Joseph Han <top2topii@gmail.com>
2020-03-03 01:03:38 +0900

    2강 zip > sorted까지 공부

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
  - [1ambda haskell](https://1ambda.github.io/haskell/)
 
 ### 어디까지 공부했더라?
- - 2강 zip > pairs
+ - 2강 zip > sorted
--- guards.hs
+++ guards.hs
@@ -9,3 +9,9 @@ prime n = factors n == [1, n]
 
 primes :: Int -> [Int]
 primes n = [x | x <- [1..n], prime x]
+
+pairs :: [a] -> [(a, a)]
+pairs xs = zip xs (tail xs)
+
+sorted :: Ord a => [a] -> Bool
+sorted xs = and [x <= y | (x, y) <- pairs xs]
